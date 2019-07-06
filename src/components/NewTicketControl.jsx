c42f2789dfc4922e36ef7a038eb6185889e27dd5
Heather Zurek <heather.zurek@gmail.com>
2019-07-06 10:40:23 -0700

    newticketcontrol component and routing

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+class NewTicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <p>This is the NewTicketControl component!</p>
+      </div>
+    );
+  }
+}
+
+export default NewTicketControl;
