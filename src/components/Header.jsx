8f21c29e2f20d0d6d03237bd487dd38e02fe7a17
Heather Zurek <heather.zurek@gmail.com>
2019-07-06 19:02:45 -0700

    added refs to the form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,17 @@ import { Link } from 'react-router-dom';
 function Header(){
   return (
     <div>
+      <style jsx>{`
+        div {
+          background-color: pink;
+          border: 1px solid black;
+          border-radius: 5px;
+          padding: 5px;
+          margin: 0 auto;
+          text-align: center;
+          width: 500px;
+        }
+        `}</style>
       <h1>Help Queue</h1>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,20 +1,52 @@
 import React from 'react';
 
 function NewTicketForm(){
+  let _names = null;
+  let _location = null;
+  let _issue = null;
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    console.log(_names.value);
+    console.log(_location.value);
+    console.log(_issue.value);
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
+  }
+
   return (
     <div>
-      <form>
+      <style jsx>{`
+        div {
+          background-color: pink;
+          border: 1px solid black;
+          border-radius: 5px;
+          padding: 5px;
+          margin: 0 auto;
+          text-align: center;
+          width: 500px;
+        }
+        button {
+          background-color: white;
+          border: 1px solid black;
+        }
+        `}</style>
+        <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/>
+          placeholder='Pair Names'
+          ref={(input) => {_names = input;}}/>
         <input
           type='text'
           id='location'
-          placeholder='Location'/>
+          placeholder='Location'
+          ref={(input) => {_location = input;}}/>
         <textarea
           id='issue'
-          placeholder='Describe your issue.'/>
+          placeholder='Describe your issue.'
+          ref={(textarea) => {_issue = textarea;}}/>
         <button type='submit'>Help!</button>
       </form>
     </div>
